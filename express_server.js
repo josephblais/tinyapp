6093bcd6659dce94b4b20d91e47d313e19beb765
Joseph Blais <josephpierreblais@gmail.com>
2021-02-15 17:43:25 -0500

    Implement generateRandomString function to create shortened URL

--- express_server.js
+++ express_server.js
@@ -1,18 +1,35 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
+
+const randomStr = function generateRandomString() {
+  // from https://attacomsian.com/blog/javascript-generate-random-string
+  return Math.random().toString(16).substr(2, 6);
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase.shortURL };
