a4433809e934ec10939903d5b67f8f088b39d3af
Joseph Blais <josephpierreblais@gmail.com>
2021-02-15 18:22:07 -0500

    redirect user to newly created page after POST submit new URL

--- express_server.js
+++ express_server.js
@@ -27,7 +27,10 @@ app.get('/urls', (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  // res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const shortURL = randomStr();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
 });
 
 
@@ -40,6 +43,7 @@ app.get("/urls/:shortURL", (req, res) => {
 app.post('/urls', (req, res) => {
   const shortURL = randomStr();
   urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.get('/', (req, res) => {
